4140754575b1ec7ccfd43d22a602fc19fb29371b
Aayush Chalise <ingoprivate@gmail.com>
2024-12-09 22:42:56 +0545

    Fix Icon Display
    Co-Authored-By: aayush <177418358+aayushrg7@users.noreply.github.com>
    Co-Authored-By: Smriti Bhandari <187972284+smritics@users.noreply.github.com>

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -6,7 +6,7 @@ export default {
   lastUpdated: true,
 
   head: [
-        ["link", { rel: "icon", href: "/carch/package.png" }],
+        ["link", { rel: "icon", href: "/docs/package.png" }],
     ],
 
   themeConfig: {
