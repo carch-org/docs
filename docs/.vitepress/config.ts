34e5aaa2e69f1022fc8af513b1a194b4a1f784b2
Hari Chalise <ingoprivate@gmail.com>
2024-12-26 21:13:22 +0545

    Carch Version 4.1.0🍀
    Co-Authored-By: aayush <177418358+aayushrg7@users.noreply.github.com>

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -22,7 +22,7 @@ export default {
       { text: "Home", link: "/" },
       { text: "Guide", link: "/getting-started/introduction" },
       { 
-        text: "4.0.0",
+        text: "4.1.0",
         items: [
           {
           text: 'Changelog',
