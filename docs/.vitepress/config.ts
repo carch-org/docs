52a5d5e2825ebbfe9c42fb4d1c76529fbf9d4ee9
Hari Chalise <ingoprivate@gmail.com>
2025-03-02 13:49:20 +0545

    Bump Version 4.1.5

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -22,7 +22,7 @@ export default {
       { text: "Home", link: "/" },
       { text: "Guide", link: "/getting-started/introduction" },
       { 
-        text: "4.1.4",
+        text: "4.1.5",
         items: [
           {
           text: 'Changelog',
