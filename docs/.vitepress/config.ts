306b8a22ccc99e6d5bce0462182dce396d17460e
Hari Chalise <ingoprivate@gmail.com>
2025-04-01 00:56:19 +0545

    Add Commands & Bump Version 4.2.6

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -22,7 +22,7 @@ export default {
       { text: "Home", link: "/" },
       { text: "Guide", link: "/getting-started/introduction" },
       { 
-        text: "4.2.5",
+        text: "4.2.6",
         items: [
           {
           text: 'Changelog',
