63a239f13f33e071fefd893bf4e32d9c6f2f3994
Hari Chalise <ingoprivate@gmail.com>
2025-05-12 00:52:22 +0545

    docs: update version to 4.4.4 (#24)
    Co-authored-by: harilvfs <183478078+harilvfs@users.noreply.github.com>

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -22,7 +22,7 @@ export default {
       { text: "Home", link: "/" },
       { text: "Guide", link: "/getting-started/introduction" },
       { 
-        text: "4.4.3",
+        text: "4.4.4",
         items: [
           {
           text: 'Changelog',
