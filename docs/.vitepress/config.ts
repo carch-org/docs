ed328f7fb4db54e67f9201ce59c8e9e6f0c0745b
Hari Chalise <ingoprivate@gmail.com>
2025-05-31 16:20:23 +0545

    chore(release): update version to 5.1.0 (#37)
    Co-authored-by: harilvfs <183478078+harilvfs@users.noreply.github.com>

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -69,7 +69,7 @@ const nav = (lang = "", override = {}) => {
     { text: override.home || "Home", link: lang ? `/${lang}/` : "/" },
     { text: override.guide || "Guide", link: guidePath },
     {
-      text: "5.0.0",
+      text: "5.1.0",
       items: [
         {
           text: override.changelog || "Changelog",
