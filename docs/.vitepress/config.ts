8e3ab83e828ef4722d0739712e485dc5f3c20299
Hari Chalise <ingoprivate@gmail.com>
2025-06-20 15:15:22 +0545

    chore(release): update version to 5.1.5 (#45)

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -71,7 +71,7 @@ const nav = (lang = "", override = {}) => {
     { text: override.home || "Home", link: lang ? `/${lang}/` : "/" },
     { text: override.guide || "Guide", link: guidePath },
     {
-      text: "5.1.4",
+      text: "5.1.5",
       items: [
         {
           text: override.changelog || "Changelog",
