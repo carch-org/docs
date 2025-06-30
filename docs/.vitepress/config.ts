5d3afdd3777052e261a6b11c96ba51b90fd1f64c
harilvfs <183478078+harilvfs@users.noreply.github.com>
2025-06-30 17:16:41 +0000

    chore(release): update version to 5.1.6

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -71,7 +71,7 @@ const nav = (lang = "", override = {}) => {
     { text: override.home || "Home", link: lang ? `/${lang}/` : "/" },
     { text: override.guide || "Guide", link: guidePath },
     {
-      text: "5.1.5",
+      text: "5.1.6",
       items: [
         {
           text: override.changelog || "Changelog",
